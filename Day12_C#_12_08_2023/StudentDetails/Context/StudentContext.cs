73e80d77f49fd9fae79e0a83e0822f19540a4068
Sanjaikumar S <141032684+sanjai-2001@users.noreply.github.com>
2023-08-13 09:27:44 +0530

    Day12

---
+++ Day12_C#_12_08_2023/StudentDetails/Context/StudentContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentDetails.Models;
+
+namespace StudentDetails.Context
+{
+    public class StudentContext:DbContext
+    {
+
+       public DbSet<Student>Students { get; set; }
+
+
+        public StudentContext(DbContextOptions<StudentContext> options) : base(options) { }
+       
+       
+    }
+}
---
+++ Day12_C#_12_08_2023/StudentDetailsDBF/Services/Interfaces/IStudent.cs
@@ -0,0 +1,16 @@
+﻿
+using StudentDetailsDBF.Models;
+
+namespace StudentDetails.Services.Interfaces
+{
+    public interface IStudent
+    {
+
+         Task<List<Student>> GetAllStudents();
+        Task<Student> GetStudentsByRollno(int rollno);
+        Task<List<Student>> AddStudent(Student student);
+        Task<List<Student>> UpdateStudent(int rollno,Student student);
+        Task<List<Student>> DeleteStudent(int rollno);  
+
+    }
+}
